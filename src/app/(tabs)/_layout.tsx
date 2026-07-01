4c82d3daecf20b21695ed02586ad5e1e7f56c3f0
FernandoGomesK <fernandogomest94@gmail.com>
2026-06-30 21:21:26 -0400

    start log

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import { Tabs } from 'expo-router';
-
-export default function TabLayout() {
-  return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: '#22C358FF', tabBarInactiveTintColor: '#565D6DFF', headerShown: false }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="talhoes"
-        options={{
-          title: 'Talhões',
-          tabBarIcon: ({ color }) => <FontAwesome5 size={28} name="layer-group" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="insumos"
-        options={{ href: null }}
-      />
-      <Tabs.Screen
-        name="maquinas"
-        options={{ href: null }}
-      />
-      <Tabs.Screen
-        name="new"
-        options={{
-          title: 'Novo',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="plus-circle" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="relatorios"
-        options={{
-          title: 'Relatórios',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="bar-chart" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Perfil',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
-=======
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import { Tabs } from 'expo-router';
@@ -82,6 +27,14 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="insumos"
+        options={{ href: null }}
+      />
+      <Tabs.Screen
+        name="maquinas"
+        options={{ href: null }}
+      />
+      <Tabs.Screen
         name="culturas"
         options={{
           title: 'Culturas',
@@ -105,4 +58,3 @@ export default function TabLayout() {
     </Tabs>
   );
 }
->>>>>>> origin/julia
